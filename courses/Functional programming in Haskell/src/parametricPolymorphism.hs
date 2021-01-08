1989163a3542f79874f18a69378a717742501826
Konstantin <36280804+Burconst@users.noreply.github.com>
2021-01-08 22:25:41 +0300

    Update

--- courses/Functional programming in Haskell/src/parametricPolymorphism.hs
+++ courses/Functional programming in Haskell/src/parametricPolymorphism.hs
@@ -4,9 +4,15 @@ import Data.Function
 getSecondFrom :: t -> t2 -> t1 -> t2
 getSecondFrom x y z = y
 
-multSecond = g `on` h
-g = (*)
-h = snd
+multSecond = g1 `on` h1
+g1 = (*)
+h1 = snd
 
 on3 :: (b -> b -> b -> c) -> (a -> b) -> a -> a -> a -> c
 on3 op f x y z = op (f x) (f y) (f z)
+
+doItYourself = f . g . h
+f = (logBase 2)
+g = (^3)
+h = (max 42)
+
